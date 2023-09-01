7cbff13b7a1c717dca3fe922bc417078ae4e68bb
Mehdi-Amiri-Dev <mahdiamiridev@gmail.com>
2023-09-01 13:17:37 +0430

    Complete Registration Form

---
+++ MyEshop/DataLayer/ViewModels/AccountViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.ViewModels
+{
+     public class RegisterViewModel
+    {
+        [Display(Name ="نام کاربری")]
+        [Required(ErrorMessage ="لطفا {0} را وارد کنید")]
+        public string UserName { get; set; }
+
+        [Display(Name = "ایمیل")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [EmailAddress(ErrorMessage ="ایمیل وارد شده معتبر نمی باشد")]
+        public string Email { get; set; }
+
+        [Display(Name = "کلمه عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "تکرار کلمه عبور")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید")]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage ="تکرار کلمه عبور با کلمه عبور مغایرت دارد")]
+        public string Repassword { get; set; }
+    }
+}
--- MyEshop/MyEshop/Controllers/AccountController.cs
+++ MyEshop/MyEshop/Controllers/AccountController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DataLayer.ViewModels;
+using DataLayer;
+using System.Web.Security;
 
 namespace MyEshop.Controllers
 {
     public class AccountController : Controller
     {
+        MyEshop_DataBaseEntities db = new MyEshop_DataBaseEntities();
+
         // GET: Account
         public ActionResult Index()
         {
@@ -19,9 +24,56 @@ namespace MyEshop.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Register (RegisterViewModel register)
+        {
+            if (ModelState.IsValid)
+            {
+                var ExistsEmail = db.Users.Any(u => u.Emain == register.Email.Trim().ToLower());
+                var ExistsUserName = db.Users.Any(u => u.UserName == register.UserName.Trim().ToLower());
+
+                if (ExistsEmail || ExistsUserName)
+                {
+                    if (ExistsEmail)
+                    {
+                        ShowRegisterErrorMessage("Email","ایمیل");
+                    }
+                    if (ExistsUserName)
+                    {
+                        ShowRegisterErrorMessage("UserName", "نام کاربری");
+                    }
+                }
+                else
+                {
+                    Users user = new Users()
+                    {
+                        UserName = register.UserName,
+                        Emain = register.Email,
+                        IsActive = false,
+                        RoleID = 1,
+                        RegisterDate = DateTime.Now,
+                        Password = FormsAuthentication.HashPasswordForStoringInConfigFile(register.Password, "MD5"),
+                        ActiveCode = Guid.NewGuid().ToString()
+                    };
+
+                    db.Users.Add(user);
+                    db.SaveChanges();
+                    return View("SuccessRegister", user);
+                }
+            }
+
+            return View(register);
+        }
+
         public ActionResult Login()
         {
             return View();
         }
+
+        public void ShowRegisterErrorMessage(string properyName , string propertyNameString)
+        {
+            ModelState.AddModelError(properyName , $"کاربر گرامی {propertyNameString} وارد شده قبلا در سایت ثبت نام شده است");
+        }
+
     }
 }
