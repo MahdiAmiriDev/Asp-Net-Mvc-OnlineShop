5b72c736d91559bc7101b02d3ecfcd6ff3a363b3
Mehdi-Amiri-Dev <mahdiamiridev@gmail.com>
2023-09-29 21:17:16 +0330

    Add Search Products Add Search by Tags

---
+++ MyEshop/MyEshop/Controllers/SearchController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataLayer;
+
+namespace MyEshop.Controllers
+{
+    public class SearchController : Controller
+    {
+
+        MyEshop_DataBaseEntities db = new MyEshop_DataBaseEntities();
+
+        // GET: Search
+
+        public ActionResult Index(string key)
+        {
+            List<Products> productList = new List<Products>();
+
+            productList.AddRange(db.Product_Tags.Where(x => x.Tag.Contains(key) )
+                                .Select(x => x.Products).ToList());
+
+            productList.AddRange(db.Products.Where(x => x.ProductTitle.Contains(key) ||
+                                                        x.ShortDescription.Contains(key) ||
+                                                        x.Text.Contains(key)));
+
+            ViewBag.Search = key;
+
+            return View(productList.Distinct());
+        }
+    }
+}
